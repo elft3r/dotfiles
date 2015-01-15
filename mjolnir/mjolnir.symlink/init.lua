ff3c44c77bc11df47796f09a62acdcb371f68bb2
Jochen Zehnder <jz@netit-services.com>
2015-01-15 10:37:50 +0100

    added 'next' and 'previous' shortcut for spotify

--- mjolnir/mjolnir.symlink/init.lua
+++ mjolnir/mjolnir.symlink/init.lua
@@ -29,10 +29,11 @@ local gridset = function(x, y, w, h)
 end
 
 local mash = {"ctrl", "shift"}
-hotkey.bind(mash, 'n', grid.pushwindow_nextscreen)
 hotkey.bind(mash, 'a', gridset(0, 0, gw/2, gh)) -- left half
 hotkey.bind(mash, 's', grid.maximize_window)
 hotkey.bind(mash, 'd', gridset(gw/2, 0, gh/2, gh)) -- right half
 hotkey.bind(mash, 't', gridset(gw/2, 0, gw/2, gh/2)) -- top right
 hotkey.bind(mash, 'b', gridset(gw/2, gh/2, gw/2, gh/2)) -- top right
 hotkey.bind(mash, 'space', spotify.play)
+hotkey.bind(mash, 'n', spotify.next)
+hotkey.bind(mash, 'p', spotify.previous)
