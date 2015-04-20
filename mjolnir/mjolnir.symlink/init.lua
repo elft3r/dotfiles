2aed2896bce5e02dc018e8f3174ab3b13a2044b5
Jochen Zehnder <Jochen.Zehnder@gmail.com>
2015-04-20 08:31:19 +0200

    updated the script to also place single applications

--- mjolnir/mjolnir.symlink/init.lua
+++ mjolnir/mjolnir.symlink/init.lua
@@ -9,7 +9,7 @@ local grid = require "mjolnir.bg.grid"
 local spotify = require "mjolnir.lb.spotify"
 
 -- set grid size and related variables
-grid.GRIDWIDTH = 6
+grid.GRIDWIDTH = 8
 grid.GRIDHEIGHT = 8
 grid.MARGINX = 0
 grid.MARGINY = 0
@@ -30,10 +30,16 @@ end
 
 local mash = {"ctrl", "shift"}
 hotkey.bind(mash, 'a', gridset(0, 0, gw/2, gh)) -- left half
-hotkey.bind(mash, 's', grid.maximize_window)
 hotkey.bind(mash, 'd', gridset(gw/2, 0, gh/2, gh)) -- right half
 hotkey.bind(mash, 't', gridset(gw/2, 0, gw/2, gh/2)) -- top right
-hotkey.bind(mash, 'b', gridset(gw/2, gh/2, gw/2, gh/2)) -- top right
+hotkey.bind(mash, 'b', gridset(gw/2, gh/2, gw/2, gh/2)) -- bottom right
+
+-- application positions
+hotkey.bind(mash, 's', gridset(0, 0, 4, 6)) -- skype window
+hotkey.bind(mash, 'c', gridset(1, 0, 7, gh)) -- chrome window
+hotkey.bind(mash, 'e', gridset(1, 1, 6, 6)) -- spotify window
+
+-- spotify commands
 hotkey.bind(mash, 'space', spotify.play)
 hotkey.bind(mash, 'n', spotify.next)
 hotkey.bind(mash, 'p', spotify.previous)
