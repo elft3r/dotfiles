9c1751a0a1017a43eac140c68874e436cca01816
Jochen Zehnder <Jochen.Zehnder@gmail.com>
2015-05-28 10:22:22 +0200

    made changes in the GOPATH configuration

--- go/path.zsh
+++ go/path.zsh
@@ -1,2 +1,8 @@
-export GOPATH=$PROJECTS/go
-export PATH="$GOPATH/bin:$PATH"
+go_home=/Users/elft3r/Coding/go-home
+export GOPATH=$go_home
+export PATH=$PATH:$go_home/bin
+
+dockerfiles=/Users/elft3r/Coding/workspace/dockerfiles
+export GOPATH=$GOPATH:$dockerfiles
+export PATH=$PATH:$dockerfiles/bin
+
