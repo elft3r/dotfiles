cd04acc20ef8166a82cc413c810a29db0c4f25d2
Jochen Zehnder <Jochen.Zehnder@gmail.com>
2015-10-19 17:29:36 +0200

    updated the dotfiles

--- mjolnir/install.sh
+++ mjolnir/install.sh
@@ -1,4 +1,5 @@
 # You have to install mjolnir manually from http://mjolnir.io/
+echo 'rocks_servers = { "http://rocks.moonscript.org" }' >> /usr/local/etc/luarocks52/config-5.2.lua
 
 # Install all needed mjolnir packages
 luarocks install mjolnir.alert
