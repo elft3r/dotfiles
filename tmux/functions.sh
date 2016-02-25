322985f82e591d9a93c81bd5f1fceee6a29239eb
Jochen Zehnder <Jochen.Zehnder@gmail.com>
2016-02-25 11:17:42 +0100

    shortened function names

--- tmux/functions.sh
+++ tmux/functions.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-tmux_main() {
+tm_main() {
 	tmux new -s main -d
 	tmux split-window -h -t main
 	tmux split-window -v -t main
 	tmux attach -t main
 }
 
-tmux_nv() {
+tm_nv() {
 	dir=${PWD##*/}
 	tmux new -s $dir -d
 	tmux send-keys -t $dir 'nv' C-m
