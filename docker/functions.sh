575b887a239084066c6aebc51794573f21d73d2f
Jochen Zehnder <Jochen.Zehnder@gmail.com>
2016-03-29 10:54:47 +0200

    added function to unset docker variables and changed naming

--- docker/functions.sh
+++ docker/functions.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-dcleanup() {
+d_cleanup() {
 	docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
 	docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
+}
+
+d_unset() {
+	unset DOCKER_TLS_VERIFY
+	unset DOCKER_CERT_PATH
+	unset DOCKER_MACHINE_NAME
+	unset DOCKER_HOST
 }
