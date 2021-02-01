a654b953a4f1c16f1000782b28d979ff39bb2d60
Jochen Zehnder <jochen.zehnder@gmail.com>
2021-02-01 11:01:59 +0100

    added functions for elgato key light

---
+++ elgato/functions.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+light_call_body() {
+cat <<EOF
+{
+	"numberOfLights":1,
+	"lights": [
+		{
+			"on": 1,
+			"brightness": 21,
+			"temperature":294
+		}
+	]
+}
+EOF
+}
+
+light_day_body() {
+cat <<EOF
+{
+	"numberOfLights":1,
+	"lights": [
+		{
+			"on": 1,
+			"brightness": 66,
+			"temperature":174
+		}
+	]
+}
+EOF
+}
+
+light_off_body() {
+cat <<EOF
+{
+	"numberOfLights":1,
+	"lights": [
+		{
+			"on": 0,
+			"brightness": 21,
+			"temperature":294
+		}
+	]
+}
+EOF
+}
+
+update_key_light() {
+	BODY=$1
+
+	curl -i \
+		-H "Accept: application/json" \
+		-H "Content-Type: application/json" \
+		-X PUT \
+		--data "$BODY" \
+		"http://192.168.1.22:9123/elgato/lights"
+}
+
+elgato_light_call() {
+	update_key_light "$(light_call_body)"
+}
+
+elgato_light_day() {
+	update_key_light "$(light_day_body)"
+}
+
+elgato_light_off() {
+	update_key_light "$(light_off_body)"
+}
