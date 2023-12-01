3e7ab03cbbae34b0c9bf037ca6869a04ba496ada
elft3r <jochen.zehnder@gmail.com>
2023-12-01 11:10:30 +0100

    enhanced terraform / terragrunt cleanup

--- coding/functions.sh
+++ coding/functions.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 clear_terragrunt_cache() {
+	find . -type d -name ".terraform" -prune -exec rm -rf {} \;
 	find . -type d -name ".terragrunt-cache" -prune -exec rm -rf {} \;
 	find . -type f -name ".terraform.lock.hcl"  -prune -exec rm -rf {} \;
 }
